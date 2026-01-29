9e51d2df9f63e4a5a0b1921c4cc11dc7e32e161d
SunStupic <sunfeng32@qq.com>
2026-01-29 13:46:57 +0800

    feat: enable agentation in production

--- apps/web/src/components/AgentationWrapper.tsx
+++ apps/web/src/components/AgentationWrapper.tsx
@@ -3,10 +3,5 @@
 import { Agentation } from 'agentation'
 
 export function AgentationWrapper() {
-  // 只在开发环境显示
-  if (process.env.NODE_ENV !== 'development') {
-    return null
-  }
-  
   return <Agentation />
 }
